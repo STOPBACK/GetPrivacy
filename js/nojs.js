87ef06cd7fe50d1b6a3bdb8c817a2d02b48b0b8c
Biltu Das <65584801+BiltuDas1@users.noreply.github.com>
2021-11-10 10:53:24 +0530

    Updated Hostname

--- js/nojs.js
+++ js/nojs.js
@@ -1,12 +1,12 @@
 url = window.location.href;
 url = url.split('?ref=/')[1];
-if ( url == "undefined" ) {
-  url = "https://getprivacy.cf"
-  window.location.replace(url);
+hostname = "https://" + window.location.hostname + "/";
+if (url == "undefined") {
+  window.location.replace(hostname);
 } else {
-  url = "https://getprivacy.cf/" + url
-  if ( url == "https://getprivacy.cf/undefined" ) {
-    url = "https://getprivacy.cf"
+  url = hostname + url;
+  if (url == hostname + "/undefined") {
+    url = hostname;
   }
   window.location.replace(url);
 }
