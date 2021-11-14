188de39a7a165931c8b5ad6f1388b632b7b8d69b
Biltu Das <65584801+BiltuDas1@users.noreply.github.com>
2021-11-14 22:26:37 +0530

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,5 @@
+include('js/cname.js');
 url = window.location.href;
 url_find = url.includes("https")
-url_find1 = url.includes("getprivacy.cf")
+url_find1 = url.includes(cname)
 url_result = url_find + url_find1;
