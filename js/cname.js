08aa2024e87c7c1d3d436fe56731315624f49f8a
Biltu Das <65584801+BiltuDas1@users.noreply.github.com>
2021-11-15 17:46:58 +0530

    Update cname.js

--- js/cname.js
+++ js/cname.js
@@ -1,1 +1,1 @@
-var txtFile=new XMLHttpRequest;txtFile.open("GET","/Cname",!0),txtFile.onreadystatechange=function(){if(4===txtFile.readyState&&200===txtFile.status)txtFile.responseText},txtFile.send(null);
+var txtFile=new XMLHttpRequest;txtFile.open("GET","../Cname",!0),txtFile.onreadystatechange=function(){if(4===txtFile.readyState&&200===txtFile.status)txtFile.responseText},txtFile.send(null);
