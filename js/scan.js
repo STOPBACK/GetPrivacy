8d73f7e825c3fc84cbe20808bac17738e83d9632
Biltu Das <65584801+BiltuDas1@users.noreply.github.com>
2021-11-15 18:54:12 +0530

    Added IP Address detection

--- js/scan.js
+++ js/scan.js
@@ -154,6 +154,49 @@ if(ads){
 }
 document.getElementById("adblocker2").innerHTML = adblocker2;
 
+     // IP Address Detection
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    //compatibility for firefox and chrome
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+
+     //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp, noop, noop);
+    }, noop); 
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+}
+
+// Usage
+
+getUserIP(function(ip){
+		document.getElementById("ip").innerHTML = ip;
+});
+
     // Download Report
   function reportdownload(){
     function download(filename, text) {
@@ -174,7 +217,8 @@ let report = 'Operating System,' + OSName + "\n" +
              'User-Agent,' + '"' + useragent + '"' + "\n" +
              'Do Not Track,' + dnt + "\n" +
              'Adblocker (Image Method),' + adblocker1 + "\n" +
-             'Adblocker (Script Method),' + adblocker2;
+             'Adblocker (Script Method),' + adblocker2 + "\n" +
+             'IP Address,' + ip;
 
       download("report.csv",report);
   }
